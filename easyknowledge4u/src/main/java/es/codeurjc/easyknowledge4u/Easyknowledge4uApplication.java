679b227f5818bf0f86241eae0f9d326227e1ea47
jpbernal <jp.bernal.2018@alumnos.urjc.es>
2021-02-21 14:58:14 +0100

    Project

---
+++ easyknowledge4u/src/main/java/es/codeurjc/easyknowledge4u/Easyknowledge4uApplication.java
@@ -0,0 +1,13 @@
+package es.codeurjc.easyknowledge4u;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Easyknowledge4uApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Easyknowledge4uApplication.class, args);
+	}
+
+}
---
+++ easyknowledge4u/src/test/java/es/codeurjc/easyknowledge4u/Easyknowledge4uApplicationTests.java
@@ -0,0 +1,13 @@
+package es.codeurjc.easyknowledge4u;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Easyknowledge4uApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
