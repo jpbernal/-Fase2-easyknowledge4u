2f8b5165dcfab809ad59ad9e7ea4a0af6fc9f4fa
jpbernal <jp.bernal.2018@alumnos.urjc.es>
2021-02-22 12:59:48 +0100

    LogController

---
+++ easyknowledge4u/src/main/java/es/codeurjc/easyknowledge4u/LogController.java
@@ -0,0 +1,25 @@
+package es.codeurjc.easyknowledge4u;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import ch.qos.logback.classic.Logger;
+
+public class LogController {
+
+	private Logger log = (Logger) LoggerFactory.getLogger(LogController.class);
+	
+	@GetMapping("/page_log")
+	public String page(Model model) {
+		
+		
+		log.trace("Trace message");
+		log.debug("Debug message");
+		log.info("Info message");
+		log.error("ErrOr message");
+		
+		
+		return "page";
+	}
+}
