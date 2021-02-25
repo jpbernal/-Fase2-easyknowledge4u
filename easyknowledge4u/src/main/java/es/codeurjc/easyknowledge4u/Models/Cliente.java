2ed0e51a007c7800c9b2b34109ad822107dccbdb
jpbernal <jp.bernal.2018@alumnos.urjc.es>
2021-02-25 12:27:13 +0100

    update

---
+++ easyknowledge4u/src/main/java/es/codeurjc/easyknowledge4u/Models/Cliente.java
@@ -0,0 +1,16 @@
+package es.codeurjc.easyknowledge4u.Models;
+
+public class Cliente {
+	
+	private Long id;
+	private String nombre;
+	private String nick;
+	private String correo;
+	private String password;
+	private String direccion;
+	//private Cursos[] cursos;
+	
+	
+	
+
+}
