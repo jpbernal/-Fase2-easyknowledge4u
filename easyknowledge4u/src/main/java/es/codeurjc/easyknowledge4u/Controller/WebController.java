1545b9528d2e5ae8fedcc388f7da371b772ae1b8
jpbernal <jp.bernal.2018@alumnos.urjc.es>
2021-02-25 13:43:00 +0100

    update

--- easyknowledge4u/src/main/java/es/codeurjc/easyknowledge4u/Controller/WebController.java
+++ easyknowledge4u/src/main/java/es/codeurjc/easyknowledge4u/Controller/WebController.java
@@ -1,13 +1,20 @@
 package es.codeurjc.easyknowledge4u.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import es.codeurjc.easyknowledge4u.Repositories.ClienteRepository;
+
 @Controller
 public class WebController {
+	
+	
+	@Autowired
+	private ClienteRepository clientes;
 	
 	@GetMapping("/index")
 	public String Index (Model model) {
