e402297fe8941397b9b710ba3168ba0bb8cb83f3
jpbernal <jp.bernal.2018@alumnos.urjc.es>
2021-03-15 14:00:36 +0100

    update

--- easyknowledge4u/src/main/java/es/codeurjc/easyknowledge4u/Models/Contacto.java
+++ easyknowledge4u/src/main/java/es/codeurjc/easyknowledge4u/Models/Contacto.java
@@ -1,14 +1,14 @@
 package es.codeurjc.easyknowledge4u.Models;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 
-
+@Entity
 public class Contacto {
 	
-
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
