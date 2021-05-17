8efb76528a3f9064372fa94fc10a778922d32c69
Juan Pablo Bernal <jp.bernal.2018@alumnos.urjc.es>
2021-05-17 17:29:05 +0200

    update Fase4
    Actualizado hazelcast, haproxy y todos los servicios dockerizados

--- easyknowledge4u/src/main/java/es/codeurjc/easyknowledge4u/Easyknowledge4uApplication.java
+++ easyknowledge4u/src/main/java/es/codeurjc/easyknowledge4u/Easyknowledge4uApplication.java
@@ -2,6 +2,10 @@ package es.codeurjc.easyknowledge4u;
 
 
 import org.apache.commons.logging.LogFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.commons.logging.Log;
 
 import org.springframework.boot.SpringApplication;
@@ -26,5 +30,19 @@ public class Easyknowledge4uApplication {
     	LOGGER.info("...");
     	return new ConcurrentMapCacheManager("cacheManager");
     }
+	
+	/*@Bean
+    public Config config() {
+    Config config = new Config();
+    JoinConfig joinConfig = config.getNetworkConfig().getJoin();
+    joinConfig.getMulticastConfig().setEnabled(false);
 
+    List<String> serversList = new ArrayList<String>();
+    serversList.add("easyknowledge4u");
+    serversList.add("easyknowledge4u2");
+
+    joinConfig.getTcpIpConfig().setEnabled(true).setMembers(serversList);
+    return config;
+    }
+*/
 }
